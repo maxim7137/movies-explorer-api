f248a3c8a8eb51d8ff10758a7b3fbc0b7eacf7d9
maxim713 <maxim713@mail.ru>
2023-01-28 17:22:12 +0300

    feature: Создайте роуты

---
+++ routes/movies.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+/*
+const {
+  validateMovieId,
+  validateMovieCreate,
+} = require('../middlewares/validations');
+ */
+const {
+  getMovies,
+  createMovie,
+  deleteMovie,
+} = require('../controllers/movies');
+
+router.get('/', getMovies); // возвращает все сохранённые текущим  пользователем фильмы
+router.post('/', /* validateMovieCreate, */ createMovie); // createMovie создаёт фильм с переданными в теле
+// country,director,duration,year,description,image,trailer,nameRU,nameEN,thumbnail,movieId
+router.delete('/:movieId', /* validateMovieId, */ deleteMovie); // удаляет сохранённый фильм по id
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+/*
+const { validateUpdateUser } = require('../middlewares/validations');
+ */
+const { getUser, updateUser } = require('../controllers/users');
+
+router.get('/me', getUser); // возвращает информацию о пользователе (email и имя)
+router.patch('/me', /* validateUpdateUser, */ updateUser); // обновляет информацию о пользователе (email и имя)
+
+module.exports = router;
